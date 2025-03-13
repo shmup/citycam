b4614ac8376a66d762639c424a36ff321063771a
Jared Tyler Miller <jared@smell.flowers>
2025-03-13 17:25:00 -0400

    Set wallpaper to crop

--- src/utils.rs
+++ src/utils.rs
@@ -9,9 +9,12 @@ pub fn get_cache_dir() -> Result<PathBuf> {
 }
 
 pub fn set_wallpaper(path: &Path) -> Result<()> {
-    wallpaper::set_from_path(path.to_str().unwrap())
-        .map_err(|e| anyhow!("Failed to set wallpaper: {}", e))?;
+    let path_str = path.to_str().ok_or_else(|| anyhow!("Invalid path"))?;
+
+    wallpaper::set_from_path(path_str).map_err(|e| anyhow!("Failed to set wallpaper: {}", e))?;
+
+    wallpaper::set_mode(wallpaper::Mode::Crop)
+        .map_err(|e| anyhow!("Failed to set wallpaper mode: {}", e))?;
 
     Ok(())
 }
-
